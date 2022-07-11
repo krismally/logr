3b1a0aa2d46db797846170f3dcb2e34a7d4d520f
Kris Mally <kristophermally@gmail.com>
2022-07-11 10:40:35 -0600

    Finish user authentication functionality

--- logr/settings.py
+++ logr/settings.py
@@ -127,6 +127,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Redirect after successful login
+# Change to dashboard once created
+LOGIN_REDIRECT_URL= '/'
+
+# Redirect after logout
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # creates a user form object
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # adds user to database
+            user = form.save()
+            # logs in user
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # either bad POST or a GET request, render signup.html with empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
