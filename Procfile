ecb068280e92611eb08743b27475975e2d7eca3c
Kris Mally <kristophermally@gmail.com>
2022-07-13 08:41:46 -0600

    Config deployement

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn logr.wsgi
--- logr/settings.py
+++ logr/settings.py
@@ -138,3 +138,8 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# configure django app for heroku
+import django_heroku
+
+django_heroku.settings(locals())
